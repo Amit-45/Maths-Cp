e3024e6470bf7adb6d3d902d42f5e3f4a9b35e84
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-05-14 03:16:25 +0530

    Update prime numbers within given range.cpp

--- Sieve of eratosthenes/prime numbers within given range.cpp
+++ Sieve of eratosthenes/prime numbers within given range.cpp
@@ -1,7 +1,8 @@
+//Program to print all the prime numbers within a given range
 #include <bits/stdc++.h>
 using namespace std;
 
-void primeSieve(int n)
+void printPrime(int n)
 {
   int prime[100] = {0};
   for (int i = 2; i <= n; i++)
@@ -25,5 +26,9 @@ int main()
   int n;
   cin >> n;
 
-  primeSieve(n);
+  printPrime(n);
 }
+
+//Time complexity : n*log(log(n)) 
+//Marked is represented by 1 and unmarked is represented by 0
+//Last loop is to print unmarked elements(Those are the required prime numbers)
